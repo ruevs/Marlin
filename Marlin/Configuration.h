6fc1ad7c0e6dad96803bed2856ad79b7048891e5
ruevs <dpr@ruevs.com>
2018-02-24 20:38:59 +0200

    Configuration updated for aluminum carriages.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -502,12 +502,12 @@
   #define DELTA_DIAGONAL_ROD 217.0 // mm
 
   // height from z=0 to home position
-  #define DELTA_HEIGHT 297.9 // get this value from auto calibrate
+  #define DELTA_HEIGHT 282.0 // get this value from auto calibrate
 
   #define DELTA_ENDSTOP_ADJ { 0.0, 0.0, 0.0 } // get these from auto calibrate
 
   // Horizontal distance bridged by diagonal push rods when effector is centered.
-  #define DELTA_RADIUS 98.5 //mm  Get this value from auto calibrate
+  #define DELTA_RADIUS 99.8 //mm  Get this value from auto calibrate
 
   // Trim adjustments for individual towers
   // tower angle corrections for X and Y tower / rotate XYZ so Z tower angle = 0
